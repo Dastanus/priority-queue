c08e2799b8161d0847357160190d32cf29e2d674
Dastanus <nixe141417@gmail.com>
2019-03-24 13:00:34 +0300

    Resolve queue

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,48 +2,150 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+    this.parentNodes = [];
+    this.sizeHeap = 0;
 	}
 
 	push(data, priority) {
-		
+    let newNode = new Node(data, priority);
+    this.sizeHeap++;
+    this.insertNode(newNode);
+    this.shiftNodeUp(newNode);  
 	}
 
 	pop() {
-		
+    let data;
+    if (this.root) {
+      this.sizeHeap--;
+      let detach = this.detachRoot();
+      data = detach.data;
+      this.restoreRootFromLastInsertedNode(detach);
+      if (this.root) {
+        this.shiftNodeDown(this.root);
+      }
+      return data;
+    }
 	}
 
 	detachRoot() {
-		
+		let rootIndex = this.parentNodes.indexOf(this.root);
+    if (rootIndex !== -1) {
+      this.parentNodes.splice(rootIndex, 1);
+    }
+
+    let detachedRoot = this.root;
+    this.root = null;
+    return detachedRoot;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+    let lastInsertedNode = this.parentNodes.pop();
+    
+    if (lastInsertedNode && detached) {
+      if (lastInsertedNode.parent
+        && lastInsertedNode.parent.right === lastInsertedNode
+        && lastInsertedNode.parent !== detached) {
+        this.parentNodes.unshift(lastInsertedNode.parent);
+      }
+      if (lastInsertedNode.parent) {
+        lastInsertedNode.parent.removeChild(lastInsertedNode);
+      }
+      lastInsertedNode.left = detached.left;
+      lastInsertedNode.right = detached.right;
+      if (lastInsertedNode.left) {
+        lastInsertedNode.left.parent = lastInsertedNode;
+      }
+      if (lastInsertedNode.right) {
+        lastInsertedNode.right.parent = lastInsertedNode;
+      }
+      if (!detached.left || !detached.right) {
+        this.parentNodes.unshift(lastInsertedNode);
+      }
+      this.root = lastInsertedNode;
+    }
 	}
 
 	size() {
-		
+		return this.sizeHeap;
 	}
 
 	isEmpty() {
-		
+		return this.sizeHeap === 0;
 	}
 
 	clear() {
-		
+    this.root = null;
+    this.sizeHeap = 0;
+    this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+    if (!this.root) {
+      this.root = node;
+      this.parentNodes.push(node);
+    } else {
+      this.parentNodes[0].appendChild(node);
+      this.parentNodes.push(node);
+      if (this.parentNodes[0].right) {
+        this.parentNodes.shift();
+      }
+    }
 	}
 
 	shiftNodeUp(node) {
-		
+    if (node.parent) {
+      if (node.parent.priority < node.priority) {
+        const nodeIndex = this.parentNodes.indexOf(node);
+        const nodeParentIndex = this.parentNodes.indexOf(node.parent);
+        if (nodeIndex !== -1) {
+          if (nodeParentIndex !== -1) {
+            const tempNode = this.parentNodes[nodeIndex];
+            this.parentNodes[nodeIndex] = this.parentNodes[nodeParentIndex];
+            this.parentNodes[nodeParentIndex] = tempNode;
+          } else {
+            this.parentNodes[nodeIndex] = node.parent;
+          }
+        }
+        node.swapWithParent();
+        this.shiftNodeUp(node);
+      }
+    } else {
+      this.root = node;
+    }
 	}
 
 	shiftNodeDown(node) {
-		
+		let child;
+    if (node.left && node.right && node.left.priority > node.right.priority) {
+      child = node.left;
+    } else if (node.left && node.right && node.left.priority <= node.right.priority) {
+      child = node.right;
+    } else if (node.left && node.left.priority > node.priority) {
+      child = node.left;
+    } else {
+      return;
+    }
+
+    let nodeIndex = this.parentNodes.indexOf(node);
+    let nodeChildIndex = this.parentNodes.indexOf(child);
+    if (nodeChildIndex !== -1) {
+      if (nodeIndex !== -1) {
+        let temp = this.parentNodes[nodeIndex];
+        this.parentNodes[nodeIndex] = this.parentNodes[nodeChildIndex];
+        this.parentNodes[nodeChildIndex] = temp;
+      } else {
+        this.parentNodes[nodeChildIndex] = node;
+      }
+    }
+    if (node === this.root) {
+      this.root = child;
+    }
+    child.swapWithParent();
+    this.shiftNodeDown(node);
 	}
 }
 
 module.exports = MaxHeap;
+
+
--- src/node.js
+++ src/node.js
@@ -1,22 +1,88 @@
 class Node {
 	constructor(data, priority) {
-
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
 	}
 
 	appendChild(node) {
-
+    if(!this.left) {
+      this.left = node;
+      this.left.parent = this;
+    } else if (!this.right){
+			this.right = node;
+			this.right.parent = this;
+    } 
+    return this;
 	}
 
 	removeChild(node) {
-
+    if(this.left === node){
+			this.left.parent = null;
+			this.left = null;
+		} else if(this.right === node) {
+			this.right.parent = null;
+			this.right = null
+		} else {
+			throw new Error("Passed node is not found");
+    }
+    return this;
 	}
 
 	remove() {
-
+    if(this.parent){
+			this.parent.removeChild(this);
+    }
+    return this;
 	}
 
 	swapWithParent() {
-		
+		let temp;
+		if(this.parent){
+			if(this.left){
+				this.left.parent = this.parent;
+			}
+			if(this.right){
+				this.right.parent = this.parent;
+			}
+			if(this.parent.left===this){
+				if (this.parent.right) {
+					this.parent.right.parent = this;
+        }
+				this.parent.left = this.left;
+				this.left = this.parent;	
+				temp = this.right;	
+				this.right = this.parent.right;
+				this.parent.right = temp;	
+			} else if(this.parent.right === this){
+				if (this.parent.left) {
+					this.parent.left.parent = this;
+				}
+        this.parent.right = this.right;	
+        this.right = this.parent;
+				temp = this.left;	
+				this.left = this.parent.left;
+				this.parent.left = temp;	
+			}
+			if(this.parent.parent){
+				if(this.parent.parent.left===this.parent){
+					this.parent.parent.left = this;
+				} else {
+					this.parent.parent.right = this;
+				}
+			}
+			temp = this.parent.parent;
+			this.parent.parent = this;
+			this.parent = temp;
+			
+		} else {
+			console.log("parent does not exist");
+		}
+    
+    return this;
+	
 	}
 }
 
